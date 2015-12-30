804cc71396f5c7321fc24a1a5d1e8c5e6fe2f251
金伟强 <iamjoel007@gmail.com>
2015-12-30 23:02:08 +0800

    add gh pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,29 @@
 import gulp from 'gulp';
+import ghPages from 'gulp-gh-pages';
 var $ = require('gulp-load-plugins')(); // 加载所有的插件
 
 gulp.task("compile", () => {
-	return gulp.src("src/**/*.js")
+    return gulp.src("src/**/*.js")
         .pipe($.sourcemaps.init())
         .pipe($.babel())
         .pipe($.sourcemaps.write("."))
         .pipe(gulp.dest("dist"));
 });
 
-var watcher = gulp.watch('src/**/*.js', ['compile']);
-watcher.on('change', (event) => {
-  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+gulp.task('watchES6', () => {
+    var watcher = gulp.watch('src/**/*.js', ['compile']);
+    watcher.on('change', (event) => {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
 
-gulp.task("default", ['compile'])
+// gh-pages 的demo的html的路径会问题
+// gulp.task('demo', () => {
+//     return gulp.src(['./dist/**/*', './src/**/*'])
+//         .pipe(ghPages());
+// });
 
+
+
+
+gulp.task("default", ['compile', 'watchES6']);
